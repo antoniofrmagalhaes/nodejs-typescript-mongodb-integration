6bcb0809063595b736e65102c4494b5f2b42d470
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-08-26 17:18:48 -0400

    Setup dotenv environment

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
 import app from './core/app';
 import appConfig from './core/config';
+
+dotenv.config();
 
 app.listen(appConfig.port, () =>
   console.log('Api running on port %s.', appConfig.port),
