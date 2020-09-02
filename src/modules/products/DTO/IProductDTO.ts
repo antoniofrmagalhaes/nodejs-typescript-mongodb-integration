36d530e39dc4a3745436bfa0ac806c45833ba6bd
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-02 13:44:24 -0400

    Added tags and amount fields to product model

--- src/modules/products/DTO/IProductDTO.ts
+++ src/modules/products/DTO/IProductDTO.ts
@@ -1,8 +1,11 @@
 export default interface IProductDTO {
+  active: boolean;
   name: string;
   category: string;
   subcategory: string;
   brand?: string;
+  amount: number;
+  tags: [string];
   description?: string;
   image?: string;
   price: number;
--- src/modules/products/infra/mongoose/entities/schemas/Product.ts
+++ src/modules/products/infra/mongoose/entities/schemas/Product.ts
@@ -12,6 +12,10 @@ export interface IProductSchema extends Document {
 
 const ProductSchema = new Schema(
   {
+    active: {
+      type: Boolean,
+      default: true,
+    },
     name: {
       type: String,
       required: true,
@@ -27,11 +31,16 @@ const ProductSchema = new Schema(
     brand: {
       type: String,
     },
+    amount: {
+      type: Number,
+    },
+    tags: [{ type: String }],
     description: {
       type: String,
     },
     image: {
       type: String,
+      default: 'image.png',
     },
     price: {
       type: Number,
