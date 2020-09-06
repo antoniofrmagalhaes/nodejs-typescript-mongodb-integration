5235f88921371a85b5b8cd66f4c155ed94e31696
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-06 09:49:10 -0400

    chore: password hash implementation removed from the user cration service

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,7 +2,7 @@ import { injectable, inject } from 'tsyringe';
 import { Document } from 'mongoose';
 
 import IUsersRepository from '../repositories/IUsersRepository';
-import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import { IUserDocument } from '../infra/mongoose/entities/schemas/User';
 
 interface Request {
   name: string;
@@ -15,18 +15,10 @@ class CreateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
-    @inject('HashProvider')
-    private hashProvider: IHashProvider,
   ) { }
 
-  public async execute({ name, email, password }: Request): Promise<Document> {
-    const passwordHash = await this.hashProvider.generateHash(password);
-
-    const user = await this.usersRepository.create({
-      name,
-      email,
-      password: passwordHash,
-    });
+  public async execute(request: Request): Promise<IUserDocument> {
+    const user = await this.usersRepository.create(request);
 
     return user;
   }
