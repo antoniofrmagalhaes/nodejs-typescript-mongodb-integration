70f62da8d467a28d1f3182dbb1be3039bb743383
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-07 11:53:21 -0400

    chore: unused methods removed from repository interface

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -3,9 +3,6 @@ import { IUserDocument } from '../infra/mongoose/entities/schemas/User';
 import ICreateUserDTO from '../DTO/ICreateUserDTO';
 
 export default interface IUsersRepository {
-  find(): Promise<IUserDocument[]>;
   create(userData: ICreateUserDTO): Promise<IUserDocument>;
-  findById(id: string): Promise<IUserDocument | null>;
-  findByEmail(email: string): Promise<IUserDocument | null>;
-  findByEmailWithPassword(email: string): Promise<IUserDocument | null>;
+  findByEmailWithPassword(email: string): Promise<IUserDocument | undefined>;
 }
