0fcba5392844dc49992f17366d22f4829c4e1079
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-07 11:49:57 -0400

    chore: index method removed

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,16 +1,9 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
-import FindUsersService from '../../../services/FindUsersService';
 import CreateUserService from '../../../services/CreateUserService';
 
 class UsersController {
-  public async index(request: Request, response: Response): Promise<Response> {
-    const findUsers = container.resolve(FindUsersService);
-    const users = await findUsers.execute();
-    return response.json(users);
-  }
-
   public async create(request: Request, response: Response): Promise<Response> {
     const createUser = container.resolve(CreateUserService);
     const user = await createUser.execute(request.body);
