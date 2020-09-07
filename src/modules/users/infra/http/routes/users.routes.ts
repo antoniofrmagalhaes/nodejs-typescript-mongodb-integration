8afd52169e7bab44b93fe63f04c8582ae706b322
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-07 11:50:56 -0400

    chore: index method removed from users routes

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -3,7 +3,6 @@ import UsersController from '../controllers/UsersController';
 
 const usersRouter = Router();
 
-usersRouter.get('/', UsersController.index);
 usersRouter.post('/', UsersController.create);
 
 export default usersRouter;
