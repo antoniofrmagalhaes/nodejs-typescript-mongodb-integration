552904a5f34e6ea64974a98cac95cf7ab00b967b
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-07 17:23:04 -0400

    test: authentication user service

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,68 @@
+import TestUsersRepository from '../infra/mongoose/repositories/test/TestUsersRepository';
+import TestHashProvider from '../providers/HashProvider/test/TestHashProvider';
+
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
+import AppError from '../../../errors/AppError';
+
+let userRepository: TestUsersRepository;
+let createUserSerivce: CreateUserService;
+let authenticateUserService: AuthenticateUserService;
+let hashProvider: TestHashProvider;
+
+describe('CreateUserService', () => {
+  beforeEach(() => {
+    userRepository = new TestUsersRepository();
+    hashProvider = new TestHashProvider();
+    createUserSerivce = new CreateUserService(userRepository);
+    authenticateUserService = new AuthenticateUserService(
+      userRepository,
+      hashProvider,
+    );
+  });
+
+  it('shoud authenticate the user', async () => {
+    await createUserSerivce.execute({
+      name: 'John Doe',
+      email: 'mail@test.com',
+      password: '12345',
+    });
+
+    const user = await authenticateUserService.execute({
+      email: 'mail@test.com',
+      password: '12345',
+    });
+
+    expect(user).toHaveProperty('token');
+  });
+
+  it('shoud not authenticate a non existing user', async () => {
+    await createUserSerivce.execute({
+      name: 'John Doe',
+      email: 'mail@test.com',
+      password: '12345',
+    });
+
+    await expect(
+      authenticateUserService.execute({
+        email: 'non-existing-email@test.com',
+        password: '12345',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('shoud not authenticate the user if the password is wrong', async () => {
+    await createUserSerivce.execute({
+      name: 'John Doe',
+      email: 'mail@test.com',
+      password: '12345',
+    });
+
+    await expect(
+      authenticateUserService.execute({
+        email: 'mail@test.com',
+        password: 'wrong',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
