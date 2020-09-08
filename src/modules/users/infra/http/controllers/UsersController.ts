1242229cfb59789761c28088f68ce92b73f4bafe
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-08 11:23:59 -0400

    chore: show user profile method created

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -2,12 +2,25 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateUserService from '../../../services/CreateUserService';
+import ShowProfileService from '../../../services/ShowProfileService';
 
 class UsersController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+    const showUser = container.resolve(ShowProfileService);
+    const profile = await showUser.execute({
+      user_id,
+    });
+    return response.json(profile);
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const createUser = container.resolve(CreateUserService);
-    const user = await createUser.execute(request.body);
-    return response.json(user);
+    await createUser.execute(request.body);
+    return response.json({
+      status: 'success',
+      message: 'user created successfully',
+    });
   }
 }
 
