f7621cf9c0205562b0ea1c0bdd9f1015a676744e
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-08 11:13:30 -0400

    feat: ensure authenticated JWT middleware

---
+++ src/modules/users/infra/middlewares/JWTAuthenticated.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../../../../config/AuthConfig';
+import AppError from '../../../../errors/AppError';
+
+interface IPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthentication(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+  if (!authHeader) {
+    throw new AppError('JWT is missing', 401);
+  }
+  const [, token] = authHeader.split(' ');
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+    const { sub } = decoded as IPayload;
+    request.user = { id: sub };
+    next();
+  } catch {
+    throw new AppError('Invalid JWT', 401);
+  }
+}
