df198ae7b398c450a9d7872987a1265c9f272470
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-08 11:24:48 -0400

    chore: get route added on users routes

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
+import JWTAuthenticated from '../../middlewares/JWTAuthenticated';
 
 const usersRouter = Router();
 
+usersRouter.use(JWTAuthenticated);
+usersRouter.get('/', UsersController.show);
 usersRouter.post('/', UsersController.create);
 
 export default usersRouter;
